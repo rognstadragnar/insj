2d91ce4e62bda00f870eaabb465463d4bd5357fa
Ragnar Rognstad <Ragnar.Rognstad@nrk.no>
2018-03-04 02:29:04 +0100

    Add eslint and fix errors

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'no-console': 'off',
+    'arrow-parens': 'off',
+  },
+};
--- index.js
+++ index.js
@@ -1,83 +1,81 @@
-const opts = require('commander')
-const inquirer = require('inquirer')
-const chalk = require('chalk')
-const path = require('path')
-const pkg = require('./package.json')
-const config = require('./config.json')
-const { cloneRepo } = require('./lib/clone')
-const { rename } = require('./lib/rename')
-const { copyAndDelete, deleteTemp } = require('./lib/copy-and-del')
-const { getData } = require('./lib/get-data')
-const { runHooks } = require('./lib/run-hooks')
-const { prompt } = require('./lib/prompt')
-const { initialPrompt } = require('./lib/initial-prompt')
-const { addTemplate } = require('./lib/add-template.js')
+const opts = require('commander');
+const chalk = require('chalk');
+const path = require('path');
+const fs = require('fs');
+const pkg = require('./package.json');
+const config = require('./config.json');
+const { cloneRepo } = require('./lib/clone');
+const { copyAndDelete, deleteTemp } = require('./lib/copy-and-del');
+const { getData } = require('./lib/get-data');
+const { runHooks } = require('./lib/run-hooks');
+const { initialPrompt } = require('./lib/initial-prompt');
+const { addTemplate } = require('./lib/add-template.js');
+
 opts
   .version(pkg.version, '-v, --version')
   .option('-r, --repo <r>')
   .option('-d, --dest <d>')
   .option('-f, --force')
   .option('-a, --add [name]')
-  .parse(process.argv)
+  .parse(process.argv);
 
-const ROOT_PATH = process.cwd()
-const TEMP_PATH = path.join(ROOT_PATH, '.tmp')
-const choices = Array.from(config.templates)
-choices.sort((a, b) => b.isDefault)
-choices.push({ name: 'Custom', value: 'custom' })
+const ROOT_PATH = process.cwd();
+const TEMP_PATH = path.join(ROOT_PATH, '.tmp');
+const choices = Array.from(config.templates);
+choices.sort((a, b) => b.isDefault);
+choices.push({ name: 'Custom', value: 'custom' });
 
 function getConfig(pathToCfg) {
   try {
     return {
+      // eslint-disable-next-line global-require, import/no-dynamic-require
       templateCfg: require(pathToCfg),
-      isTemplate: true
-    }
+      isTemplate: true,
+    };
   } catch (err) {
     return {
       templateCfg: {},
-      isTemplate: false
-    }
+      isTemplate: false,
+    };
   }
 }
 
-function throwIfNotClean(rootPath, pathToDestination, opts) {
-  let directory
+function throwIfNotClean(rootPath, pathToDestination) {
+  let directory;
   try {
-    directory = fs.readdirSync(path.resolve(ROOT_PATH, pathToDestination))
+    directory = fs.readdirSync(path.resolve(ROOT_PATH, pathToDestination));
   } catch (err) {
-    return
+    return;
   }
 
   if (directory.length > 0 && !opts.force) {
-    console.log(chalk.red('Aborting:'), 'Destination is not clean')
-    console.log('    Use', chalk.cyan('--force'), 'to override')
-    throw new Error()
+    console.log(chalk.red('Aborting:'), 'Destination is not clean');
+    console.log('    Use', chalk.cyan('--force'), 'to override');
+    throw new Error();
   }
 }
 
 async function main() {
   if (opts.add) {
-    await addTemplate(opts.add, config, path.resolve(__dirname, 'config.json'))
-    process.exit(0)
+    await addTemplate(opts.add, config, path.resolve(__dirname, 'config.json'));
+    process.exit(0);
   }
   try {
-    const { pathToDestination, pathToRepo } = await initialPrompt(opts, choices)
-    throwIfNotClean(ROOT_PATH, pathToDestination, opts)
-    await deleteTemp(TEMP_PATH)
-    await cloneRepo(pathToRepo, TEMP_PATH)
-    const { templateCfg, isTemplate } = getConfig(
-      path.join(__dirname, TEMP_PATH, 'config.js')
-    )
-    const data = isTemplate ? await getData(templateCfg) : {}
-    await copyAndDelete(TEMP_PATH, pathToDestination, data, isTemplate)
-    await runHooks(templateCfg, pathToDestination)
-    console.log('\nGo to your project by running')
-    console.log(chalk.cyan(`\n\tcd ${pathToDestination}\t`))
-    console.log()
+    const { pathToDestination, pathToRepo } = await initialPrompt(opts, choices);
+    throwIfNotClean(ROOT_PATH, pathToDestination, opts);
+    await deleteTemp(TEMP_PATH);
+    await cloneRepo(pathToRepo, TEMP_PATH);
+    const { templateCfg, isTemplate } = getConfig(path.join(TEMP_PATH, 'config.js'));
+    const data = isTemplate ? await getData(templateCfg) : {};
+    await copyAndDelete(TEMP_PATH, pathToDestination, data, isTemplate);
+    await runHooks(templateCfg, pathToDestination);
+    console.log('\nGo to your project by running');
+    console.log(chalk.cyan(`\n\tcd ${pathToDestination}\t`));
+    console.log();
   } catch (err) {
-    if (err.message) console.error(err.message)
-    process.exit(0)
+    if (err.message) console.error(err.message);
+    process.exit(0);
   }
 }
 
-main()
+main();
